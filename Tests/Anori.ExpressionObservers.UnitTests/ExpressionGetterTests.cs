9b87f167a47c1140da299fdff8b7dde691988a6e
Martin Egli <martin.egli@reishauer.com>
2021-02-26 10:16:20 +0100

    Update

--- Tests/Anori.ExpressionObservers.UnitTests/ExpressionGetterTests.cs
+++ Tests/Anori.ExpressionObservers.UnitTests/ExpressionGetterTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq.Expressions;
 using Anori.ExpressionObservers.UnitTests.TestClasses;
+using Anori.Extensions;
 using Anori.PropertyChain.UnitTest;
 using NUnit.Framework;
 // ReSharper disable RedundantAssignment
